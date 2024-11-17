0b31bf781cb79d3448531a677724c1eb9ce169e6
Veronicah <macveron100@gmail.com>
2024-11-17 07:37:38 -0800

    Duplicate alx-react-app to alx-react-app-new

--- alx-react-app-new/alx-react-app/src/App.jsx
+++ alx-react-app-new/alx-react-app/src/App.jsx
@@ -1,3 +1,8 @@
+import UserProfile from './components/UserProfile';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,6 +14,11 @@ function App() {
   return (
     <>
       <div>
+        <WelcomeMessage />
+            <Header />
+            <MainContent />
+            <Footer />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
