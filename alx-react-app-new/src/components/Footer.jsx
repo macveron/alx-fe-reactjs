39141d96860d8badaa851ea277425dc35babb0a5
Veronicah Macharia <macveron100@gmail.com>
2024-11-17 09:35:36 -0800

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,9 +1,10 @@
-function Footer() {
+const Footer = () => {
     return (
-        <footer>
-            <p>© 2023 City Lovers</p>
-        </footer>
+      <footer style={{ textAlign: 'center', padding: '10px', backgroundColor: '#222', color: '#fff' }}>
+        <p>&copy; 2024 My Favorite Cities</p>
+      </footer>
     );
-}
-
-export default Footer;
+  };
+  
+  export default Footer;
+  
