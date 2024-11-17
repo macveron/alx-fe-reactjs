9caef6a510d9afc007989607eff7407ad3f129d5
Veronicah Macharia <macveron100@gmail.com>
2024-11-17 10:30:54 -0800

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,17 @@
-const UserProfile = (props) => {
-    return (
-        <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
-        </div>
-    );
-};
+// Import necessary modules
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserProfile() {
+  // Consume the UserContext
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
 
 export default UserProfile;
