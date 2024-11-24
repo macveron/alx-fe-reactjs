be561f3cf79bf8c01b689e800da9fd9d84f734e2
Veronicah Macharia <macveron100@gmail.com>
2024-11-24 07:57:19 -0800

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,21 +3,28 @@ import create from 'zustand';
 const useRecipeStore = create((set) => ({
   recipes: [],
 
-  // Add a recipe
-  addRecipe: (newRecipe) => 
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-
-  // Delete a recipe by ID
-  deleteRecipe: (id) => 
-    set((state) => ({ recipes: state.recipes.filter((recipe) => recipe.id !== id) })),
-
-  // Update a recipe by ID
-  updateRecipe: (updatedRecipe) => 
+  // Action to add a new recipe
+  addRecipe: (newRecipe) =>
     set((state) => ({
-      recipes: state.recipes.map((recipe) => 
+      recipes: [...state.recipes, newRecipe],
+    })),
+
+  // Action to update a recipe
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
     })),
+
+  // Action to delete a recipe
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+
+  // Action to initialize or set the recipe list
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }),
 }));
 
 export default useRecipeStore;
