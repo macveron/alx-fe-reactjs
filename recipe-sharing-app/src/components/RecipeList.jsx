59f8ab2d43beff3c7a8041a51cf2ca630299822d
Veronicah Macharia <macveron100@gmail.com>
2024-11-24 08:07:36 -0800

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useRecipeStore from './recipeStore';
 
 const RecipeList = () => {
@@ -11,6 +12,10 @@ const RecipeList = () => {
           <div key={recipe.id} style={{ marginBottom: '20px' }}>
             <h3>{recipe.title}</h3>
             <p>{recipe.description}</p>
+            {/* Use Link to navigate to the recipe details page */}
+            <Link to={`/recipes/${recipe.id}`} style={{ color: 'blue', textDecoration: 'underline' }}>
+              View Details
+            </Link>
           </div>
         ))
       ) : (
