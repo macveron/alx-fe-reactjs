376cf1b4ff02b434ad5a631880f359d045d251fd
Veronicah Macharia <macveron100@gmail.com>
2024-11-24 09:38:19 -0800

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,24 +1,21 @@
-// src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from './components/Profile';
-import ProfileDetails from './components/ProfileDetails';
-import ProfileSettings from './components/ProfileSettings';
-import BlogPost from './components/BlogPost';
-import ProtectedRoute from './components/ProtectedRoute';
+import BlogPost from './components/BlogPost'; // Import the component for individual blog post
+import Home from './components/Home'; // You can add a home component
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<ProtectedRoute />}>
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
-        <Route path="/post/:id" element={<BlogPost />} />
-      </Routes>
+      <div>
+        <h1>My React App</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="profile/*" element={<Profile />} />
+          {/* Dynamic Route for Blog Post */}
+          <Route path="blog/:id" element={<BlogPost />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
