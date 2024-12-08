341b83bc8e0917cc797966a36fcf74a0835f8195
Veronicah Macharia <macveron100@gmail.com>
2024-12-08 08:01:21 -0800

    Update tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Old purge syntax
+  darkMode: "class", // Enable dark mode (optional)
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {
+      backgroundColor: ["active", "group-hover"], // Example custom variant
+    },
+  },
   plugins: [],
-}
-
+};
