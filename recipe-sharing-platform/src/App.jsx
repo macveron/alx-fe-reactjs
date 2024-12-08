b85068f23fd14981cf97e356ed11fb91a61534be
Veronicah Macharia <macveron100@gmail.com>
2024-12-08 08:15:58 -0800

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+import HomePage from "./components/HomePage";
+
 function App() {
   return (
-    <div className="text-blue-500">
-      <h1>Welcome to the Recipe Sharing Platform!</h1>
+    <div>
+      <HomePage />
     </div>
   );
 }
+
+export default App;
