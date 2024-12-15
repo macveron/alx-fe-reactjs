2e812a89d650aee80581ca63acc657ba268f3d7f
Veronicah Macharia <macveron100@gmail.com>
2024-12-15 00:57:59 -0800

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
+const GITHUB_API_BASE_URL = 'https://api.github.com/users';
+
 const fetchUserData = async (username) => {
-  const url = `https://api.github.com/users/${username}`;
   try {
-    const response = await axios.get(url);
-    return response.data; // Return user data if successful
+    const response = await axios.get(`${GITHUB_API_BASE_URL}/${username}`);
+    return response.data;
   } catch (error) {
-    throw new Error("User not found"); // Explicitly throw error for handling
+    throw new Error(error.response?.status === 404 ? 'User not found' : 'Error fetching data');
   }
 };
 
