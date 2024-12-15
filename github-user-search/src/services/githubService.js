b86522ad612498210d93ab6e249d551b55af8185
Veronicah Macharia <macveron100@gmail.com>
2024-12-15 01:20:53 -0800

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,21 @@
 import axios from 'axios';
+const GITHUB_SEARCH_API_URL = 'https://api.github.com/search/users';
 
-const GITHUB_API_BASE_URL = 'https://api.github.com/users';
-
-const fetchUserData = async (username) => {
+const fetchUserData = async ({ username, location, minRepos }) => {
   try {
-    const response = await axios.get(`${GITHUB_API_BASE_URL}/${username}`);
-    return response.data;
+    let query = `q=${username ? `user:${username}` : ''}`;
+
+    if (location) {
+      query += `+location:${location}`;
+    }
+
+    if (minRepos) {
+      query += `+repos:>=${minRepos}`;
+    }
+
+    const response = await axios.get(`${GITHUB_SEARCH_API_URL}?${query}`);
+
+    return response.data.items;
   } catch (error) {
     throw new Error(error.response?.status === 404 ? 'User not found' : 'Error fetching data');
   }
