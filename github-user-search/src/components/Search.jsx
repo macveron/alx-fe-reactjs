bf8b7cec00757d9583626985d217e8d1a2172312
Veronicah Macharia <macveron100@gmail.com>
2024-12-15 01:02:22 -0800

    Update Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -17,8 +17,7 @@ const Search = () => {
       const response = await axios.get(`https://api.github.com/users/${username}`);
       setUserData(response.data);
     } catch (err) {
-      // If error, show the error message
-      setError("Looks like we can't find the user");
+     setError("Looks like we can't find the user");
       setUserData(null);
     } finally {
       // Turn off the loading state after the request is complete
